00fea33d147a4bfc8802024da8e6db0bb927015d
Raphael Berger <raphaelmb@gmail.com>
2023-03-24 15:00:03 -0300

    bind js to rust

--- public/main.js
+++ public/main.js
@@ -1,1 +1,25 @@
-console.log('test')
+(async () => {
+  let rustApp = null
+  try {
+    rustApp = await import('../pkg')
+  } catch (err) {
+    console.error(err)
+    return
+  }
+
+  console.log(rustApp)
+
+  const input = document.getElementById('upload')
+  const fileReader = new FileReader()
+
+  fileReader.onloadend = () => {
+    let base64 = fileReader.result.replace(
+      /^data:image\/(png|jpeg|jpg);base64,/, ''
+    )
+    rustApp.grayscale(base64)
+  }
+
+  input.addEventListener('change', () => {
+    fileReader.readAsDataURL(input.files[0])
+  })
+})()
